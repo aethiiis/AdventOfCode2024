5b8615c3085ed1173b0c4f0aa701fb71f86b0623
aethiiis <137384414+aethiiis@users.noreply.github.com>
2024-12-02 09:12:30 +0100

    commit for day02

--- src/day02/day02.py
+++ src/day02/day02.py
@@ -1,13 +1,71 @@
-from collections import Counter
-
 def processing():
-    pass
+    # Extract raw data
+    with open("src/day02/input.txt", "r") as input:
+        lines = [[int(num) for num in line.split()] for line in input]
+    
+    return lines
 
 def part1():
-    pass
+    lines:list[list[int]] = processing()
+
+    # Iterate through the lines to check whether they're safe
+    sum = 0
+    for i in range(len(lines)):
+        safe = check_safe(lines[i])
+
+        if safe:
+            sum += 1
+    
+    return sum
 
 def part2():
-    pass
+    lines:list[list[int]] = processing()
+
+    # Iterate through the lines to check whether they're safe
+    sum = 0
+    for i in range(len(lines)):
+        safe = check_safe(lines[i])
+
+        # If initial check failed, check whether any sublist can pass
+        if not safe:
+            possibilites:list[list[int]] = [lines[i][:j] + lines[i][j+1:] for j in range(len(lines[i]))]
+            for j in range(len(possibilites)):
+                if check_safe(possibilites[j]):
+                    safe = True
+                    break
+
+        if safe:
+            sum += 1
+
+    return sum
+
+def check_safe(line:list) -> bool:
+    # Checks whether an individual report is safe
+    safe = True
+    increase = 0
+    security = False
+
+    for j in range(len(line) - 1):
+        if increase == 0:
+            if line[j] > line[j+1] and 1 <= abs(line[j] - line[j+1]) <= 3:
+                increase = 1
+            elif line[j] < line[j+1] and 1 <= abs(line[j] - line[j+1]) <= 3:
+                increase = -1
+            else:
+                safe = False
+                break
+        
+        elif increase == 1:
+            if line[j] < line[j+1] or abs(line[j] - line[j+1]) < 1 or abs(line[j] - line[j+1]) > 3:
+                safe = False
+                break
+
+        else:
+            if line[j] > line[j+1] or abs(line[j] - line[j+1]) < 1 or abs(line[j] - line[j+1]) > 3:
+                safe = False
+                break
+    
+    return safe
 
 if __name__ == "__main__":
     print(part1())
